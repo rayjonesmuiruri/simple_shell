29f2f554e7cf99a04ab0a4c0156dda4b62871ea7
rayjonesmuiruri <rjmuiruri@gmail.com>
2023-11-06 16:10:06 +0300

    Betty wont catch me!

---
+++ beautifulCode.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*
+ * main(void) - Entry point
+ *
+ * Description: This is the main function of the program.
+ * Prints a message and returns 0 to indicate success.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("This is a good code that passes Betty\n");
+return (0);
+}
+
